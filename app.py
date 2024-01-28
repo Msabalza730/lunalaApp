9920d4144a7760996aa47c196cced87a699f60fc
sabalza <maryori.sabalza.ext@nokia.com>
2024-01-27 21:07:05 -0500

    [FEAT] Adding html and DB conection

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/stocks'
+
+#db = SQLAlchemy(app)
+
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
 
 
 
